47f4e8a4fc7c8bb62397091fce9ee6b7a9a16d4a
marcosdellavecchia <33458523+marcosdellavecchia@users.noreply.github.com>
2020-06-26 23:11:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const name = document.getElementById('name')
-const password = document.getElementById('password')
-const form = document.getElementById('form')
-const errorText = document.getElementById('error')
+const name = document.getElementById("name");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
+const errorText = document.getElementById("error");
 
-form.addEventListener('submit', function (e) {
-    let messages = []
-    if (name.value === '' || name.value == null) {
-        messages.push('Username is required')
-    }
-    if (name.value.length < 3) {
-        messages.push('Your username needs to be at least 3 characters long')
-    }
-    if (password.value.length <= 6) {
-        messages.push('Password must be longer than 6 characters')
-    }
-    if (password.value.length >= 20) {
-        messages.push('Password must be shorter than 20 characters')
-    }
-    if (password.value === 'password') {
-        messages.push('Your password cannot be \"password\"')
-    }
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorText.innerHTML = messages.join('<br>')
-    }
-})
+form.addEventListener("submit", function (e) {
+  let messages = [];
+  if (name.value === "" || name.value == null) {
+    messages.push("Username is required");
+  }
+  if (name.value.length < 3) {
+    messages.push("Your username needs to be at least 3 characters long");
+  }
+  if (password.value.length <= 6) {
+    messages.push("Password must be longer than 6 characters");
+  }
+  if (password.value.length >= 20) {
+    messages.push("Password must be shorter than 20 characters");
+  }
+  if (password.value === "password") {
+    messages.push('Your password cannot be "password"');
+  }
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorText.innerHTML = messages.join("<br>");
+  }
+});
